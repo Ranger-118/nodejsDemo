43ae921f490320ff8c07955543bbd57a1e5f3efd
Henry Hu <henry_hu@cathaypacific.com>
2020-10-27 18:03:39 +0800

    First commit

---
+++ object/storage.js
@@ -0,0 +1,22 @@
+let item = {
+    name: null
+}
+let list = []
+let data = []
+
+
+function updateList() {
+
+}
+
+function deleteList() {
+
+}
+
+function updateItem() {
+
+}
+
+function deleteItem() {
+    
+}
